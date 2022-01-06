c4609c98f6b129d88cdfc22993526bbf7b8731d1
Username <Email address>
2022-01-06 18:01:56 +0900

    프런트 단 JavaScript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -11,6 +11,7 @@ const home = require("./src/routes/home");
 // 앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`)); // dirname -> app.js의 위치를 반환해줌, src안에 있는 public 폴더를 정적 경로로 추가해줌
 
 app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드.
 
