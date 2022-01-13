211af84e16551867a9caf96de4767989f4925659
Username <Email address>
2022-01-13 14:34:14 +0900

    회원가입 페이지 구현

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -7,6 +7,6 @@ const ctrl = require("./home.ctrl");
 
 router.get("/", ctrl.output.home);
 router.get("/login", ctrl.output.login);
-router.post("/login", ctrl.process.login);
+  router.get("/register", ctrl.output.register); // 서버 단에서 구현되고 있는 것 
 
 module.exports = router; 
